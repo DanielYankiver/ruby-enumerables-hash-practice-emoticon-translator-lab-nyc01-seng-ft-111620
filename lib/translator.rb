276f15fcc605a7a3bb1c8912dc2680b9a4bfca5f
Daniel Yankiver <dyankiver@gmail.com>
2020-11-10 15:17:52 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -40,6 +40,7 @@ def get_japanese_emoticon(path, japanese_emoticon)
     english_emoticon = emoticon_lib[key][:english]
     if jap_emoticon == japanese_emoticon
       english_emoticon
+      binding.pry
     end
     translation
   end 
