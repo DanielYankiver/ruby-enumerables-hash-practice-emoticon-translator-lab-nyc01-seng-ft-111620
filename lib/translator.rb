78a64c7c6c95954243d584549360772c595199de
Daniel Yankiver <dyankiver@gmail.com>
2020-11-10 16:49:14 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,10 +13,10 @@ def load_library(path)
   emoticon_lib
 end
 
-def get_japanese_emoticon(path, emoticon)
+def get_japanese_emoticon(path, english_emoticon)
   emoticon_lib = load_library(path)
   emoticon_lib.keys.each do |key|  # .keys = iterate over .each key 
-    if emoticon_lib[key][:english] == emoticon 
+    if emoticon_lib[key][:english] == english_emoticon 
       emoticon_lib[key][:japanese]
     end 
   end
