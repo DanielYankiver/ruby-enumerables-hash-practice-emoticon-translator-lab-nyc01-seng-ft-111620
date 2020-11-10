b981eae6844570bf1dfe14bae0877fac268ea274
Daniel Yankiver <dyankiver@gmail.com>
2020-11-10 17:20:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,7 +22,7 @@ def get_japanese_emoticon(path, english_emoticon)
   end
   if emoticon_lib[answer][:japanese]
     emoticon_lib[answer][:japanese]
-    binding.pry
+
   else 
   "Sorry, that emoticon was not found"
   end
